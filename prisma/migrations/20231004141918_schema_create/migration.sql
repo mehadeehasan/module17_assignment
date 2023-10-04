0a11327632670582741f34f2b892355ba97871ad
Md. Mehadee Hasan <69212670+mehadeehasan@users.noreply.github.com>
2023-10-04 21:09:43 +0600

    Add files via upload

---
+++ prisma/migrations/20231004141918_schema_create/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(50) NOT NULL,
+    `lastName` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `mobile` VARCHAR(50) NOT NULL,
+    `pasword` VARCHAR(50) NOT NULL,
+    `otp` VARCHAR(50) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
